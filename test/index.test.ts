b854a2415710035b0925205856a8a9f61a888326
Daniel Starns <danielstarns@hotmail.com>
2020-08-22 12:09:41 +0100

    test: index test

---
+++ test/index.test.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { expect } from "chai";
+import { describe } from "mocha";
+import { default as API } from "../src";
+
+describe("index", () => {
+  it("should export model", () => {
+    expect(API.model).to.be.a("function");
+  });
+});
