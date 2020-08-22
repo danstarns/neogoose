35c4054ed9a37871f96a16784d74a680e4234c38
Daniel Starns <danielstarns@hotmail.com>
2020-08-22 11:57:48 +0100

    test: add first tests for model

---
+++ test/methods/model.test.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { expect } from "chai";
+import { describe } from "mocha";
+import model from "../../src/methods/model";
+import { Runtime } from "../../src/types";
+
+describe("methods/model", () => {
+  describe("validation", () => {
+    it("should be a function", () => {
+      expect(model).to.be.a("function");
+    });
+
+    it("should return a function when passed runtime", () => {
+      const runtime: Runtime = {};
+
+      const _model = model(runtime);
+
+      expect(_model).to.be.a("function");
+    });
+  });
+});
