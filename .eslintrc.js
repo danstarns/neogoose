40d323a90eb2b27e1f7c56d2730af150e6f9d2a3
Daniel Starns <danielstarns@hotmail.com>
2020-08-27 13:53:38 +0100

    lint: tsban

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/ban-ts-comment": 0,
     "@typescript-eslint/explicit-function-return-type": 0,
     "prettier/prettier": [
       "error",
