3d8a39fdb7d4ccb60c867375a61b701a2f17874f
Daniel Starns <danielstarns@hotmail.com>
2020-09-01 10:03:53 +0100

    refactor: add some type comments

--- src/types.ts
+++ src/types.ts
@@ -6,20 +6,43 @@ import { IResolvers } from "@graphql-tools/utils";
 export type TypeDefsUnion = string | DocumentNode;
 
 export type Runtime = {
+  /**
+   * The models created with model
+   */
   models: Model[];
-  // The main connection
+  /**
+   * The Main connection
+   */
   connection?: Connection;
-  // Connections created with createConnection
+  /**
+   * Connections created with createConnection
+   */
   connections: Connection[];
+  /**
+   * Schema CRUD input and output is passed through
+   */
   validationSchema?: GraphQLSchema;
 };
 
 export type Resolvers = IResolvers;
 
 export interface ModelOptions {
+  /**
+   * GraphQL schema definition @see https://graphql.org/learn/schema/
+   * used to validate model CRUD operations
+   */
   typeDefs: TypeDefsUnion;
+  /**
+   * The options passed directly into driver.session @see https://github.com/neo4j/neo4j-javascript-driver
+   */
   sessionOptions?: SessionOptions;
+  /**
+   * Override the default connection for this model
+   */
   connection?: Connection;
+  /**
+   * Resolvers to attach to validation and neo4j-graphql-js schemas
+   */
   resolvers?: Resolvers;
 }
 
