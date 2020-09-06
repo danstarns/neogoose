72370a9dd7d4713c868537ac1a902e785aa6fcde
Daniel Starns <danielstarns@hotmail.com>
2020-09-06 19:31:47 +0100

    feat: add first pass at createNeoGQLSchema

--- src/graphql/create-neo-gql-schema.ts
+++ src/graphql/create-neo-gql-schema.ts
@@ -1,8 +1,93 @@
-import { GraphQLSchema } from "graphql";
+import {
+  GraphQLSchema,
+  ObjectTypeExtensionNode,
+  ObjectTypeDefinitionNode,
+  DirectiveNode,
+  print,
+  DocumentNode,
+} from "graphql";
 import { Runtime } from "../types";
+import { makeAugmentedSchema } from "neo4j-graphql-js";
+import { mergeResolvers } from "@graphql-tools/merge";
+
+function transformDirectiveToNeo4jGQL(directive: DirectiveNode): DirectiveNode {
+  if (directive.name.value === "Relationship") {
+    return {
+      ...directive,
+      name: {
+        ...directive.name,
+        value: "relation",
+      },
+      arguments: directive.arguments
+        .filter((arg) => arg.name.value !== "properties")
+        .map((arg) => {
+          if (arg.name.value === "label") {
+            return {
+              ...arg,
+              name: {
+                ...arg.name,
+                value: "name",
+              },
+            };
+          }
+
+          return arg;
+        }),
+    };
+  }
+
+  return directive;
+}
+
+function transformDefinitionToNeo4jGQL(
+  definition: ObjectTypeDefinitionNode | ObjectTypeExtensionNode
+): ObjectTypeDefinitionNode | ObjectTypeExtensionNode {
+  return {
+    ...definition,
+    fields: definition.fields.map((field) => ({
+      ...field,
+      directives: field.directives.map(transformDirectiveToNeo4jGQL),
+    })),
+    directives: definition.directives.filter(
+      (directive) => directive.name.value !== "Validation"
+    ),
+  };
+}
 
 function createNeoGQLSchema(input: { runtime: Runtime }): GraphQLSchema {
-  return {} as GraphQLSchema;
+  const document: DocumentNode = { kind: "Document", definitions: [] };
+  const resolvers = [];
+
+  input.runtime.models.forEach((model) => {
+    if (model.resolvers) {
+      resolvers.push(model.resolvers);
+    }
+
+    model.document.definitions.forEach((def) => {
+      switch (def.kind) {
+        case "ObjectTypeDefinition":
+        case "ObjectTypeExtension":
+          {
+            // @ts-ignore
+            document.definitions.push(transformDefinitionToNeo4jGQL(def));
+          }
+          break;
+
+        default:
+          // @ts-ignore
+          document.definitions.push(def);
+      }
+    });
+  });
+
+  const typeDefs = print(document);
+
+  const schema = makeAugmentedSchema({
+    typeDefs,
+    resolvers: mergeResolvers(resolvers),
+  });
+
+  return schema;
 }
 
 export = createNeoGQLSchema;
--- test/graphql/create-neo-gql-schema.test.ts
+++ test/graphql/create-neo-gql-schema.test.ts
@@ -1,11 +1,79 @@
 import { expect } from "chai";
 import { describe } from "mocha";
 import createNeoGQLSchema from "../../src/graphql/create-neo-gql-schema";
+import model from "../../src/methods/model";
+import { Runtime, Connection } from "../../src/types";
+import { printSchema } from "graphql";
 
 describe("graphql/createNeoGQLSchema", () => {
   describe("validation", () => {
     it("should be a function", () => {
       expect(createNeoGQLSchema).to.be.a("function");
+    });
+  });
+
+  describe("functionality", () => {
+    it("should", () => {
+      const runtime: Runtime = {
+        models: [],
+        connections: [],
+        // @ts-ignore
+        connection: new Connection({ config: {}, driver: {} }),
+      };
+
+      const _model = model(runtime);
+
+      _model("Movie", {
+        typeDefs: `
+          input MovieValidation {
+            title: String
+            year: Int
+            imdbRating: Float
+          }
+
+          input MovieGenres {
+            test: String
+          }
+
+          type Movie @Validation(properties: MovieValidation) {
+            title: String @id
+            year: Int
+            imdbRating: Float
+            genres: [Genre] @Relationship(label: "IN_GENRE", direction: "OUT", properties: MovieGenres)
+            test: [Movie] @cypher
+          }
+        `,
+      });
+
+      _model("Genre", {
+        typeDefs: `
+          type Genre {
+            name: String
+            movies: [Movie] @Relationship(label: "IN_GENRE", direction: "IN")
+          }
+        `,
+      });
+
+      const _createNeoGQLSchema = createNeoGQLSchema({ runtime });
+
+      const printed = printSchema(_createNeoGQLSchema);
+
+      expect(printed).to.not.include("@Relationship");
+      expect(printed).to.not.include("@Validation");
+      expect(printed).to.include("Movie");
+      expect(printed).to.include("AddMovieGenres");
+      expect(printed).to.include("RemoveMovieGenres");
+      expect(printed).to.include("MergeMovieGenres");
+      expect(printed).to.include("CreateMovie");
+      expect(printed).to.include("UpdateMovie");
+      expect(printed).to.include("DeleteMovie");
+      expect(printed).to.include("MergeMovie");
+      expect(printed).to.include("AddGenreMovies");
+      expect(printed).to.include("RemoveGenreMovies");
+      expect(printed).to.include("MergeGenreMovies");
+      expect(printed).to.include("CreateGenre");
+      expect(printed).to.include("DeleteGenre");
+      expect(printed).to.include("MergeGenre");
     });
   });
 });
