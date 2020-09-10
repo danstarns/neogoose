95eb839c78327fbd63b062db7dc4ea72e74a9016
Daniel Starns <danielstarns@hotmail.com>
2020-09-10 11:43:25 +0100

    refactor: findMany to use WHERE

--- src/neo4j/find-many.ts
+++ src/neo4j/find-many.ts
@@ -1,5 +1,6 @@
 import { Model } from "../classes";
 import { Query, FindManyOptions, SessionOptions } from "../types";
+import createWhereAndParams from "./create-where-and-params";
 
 async function findMany<T = any>({
   model,
@@ -20,32 +21,30 @@ async function findMany<T = any>({
 
   const session = connection.driver.session(sessionOptions);
 
-  const keys = Object.keys(query);
+  let params = {};
+  const match = `MATCH (n:${model.name})`;
+  let where;
+  const pagination = `
+    ${options.skip ? `SKIP ${options.skip}` : ""}
+    ${options.limit ? `LIMIT ${options.limit}` : ""}
+  `;
 
-  function createParams() {
-    let params = `{`;
+  if (Object.keys(query).length) {
+    const w = createWhereAndParams({ model, query });
 
-    for (let i = 0; i < keys.length; i++) {
-      const k = keys[i];
-      const next = keys[i + 1];
-
-      params += `${k}: $node.${k}${next ? "," : ""}`;
-    }
-
-    params += "}";
-
-    return params;
+    params = { ...w.params };
+    where = w.where;
   }
 
   const cypher = `
-      MATCH (n:${model.name} ${keys.length ? createParams() : ""})
-      RETURN n
-      ${options.skip ? `SKIP ${options.skip}` : ""}
-      ${options.limit ? `LIMIT ${options.limit}` : ""}
-    `;
+    ${match}
+    ${where}
+    RETURN n
+    ${pagination}
+  `;
 
   try {
-    const result = await session.run(cypher, { node: query });
+    const result = await session.run(cypher, params);
 
     return result.records.map((record) => record.get("n").properties);
   } catch (error) {
